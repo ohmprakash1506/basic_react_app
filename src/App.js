98ad9d5916451914431f5533f2116e0638b3a121
ohmprakash1506 <ohmprakash96@outlook.com>
2023-11-15 19:44:03 +0530

    basic react app setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route ,Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/tutorials" className="navbar-brand">
+            Konnectify
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/tutorials/add"} className="nav-link">
+                Add Tutorials
+              </Link>
+            </li>
+          </div>
+        </nav>
+        <div className="container mt-3">
+          <Routes>
+            
+          </Routes>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/common/router.js
@@ -0,0 +1,11 @@
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+
+export const withRouter = (Component) => {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let navigation = useNavigate();
+        let params = useParams();
+        return <Component {...props} router={{location, navigation, params}} />
+    }
+    return ComponentWithRouterProp
+}
